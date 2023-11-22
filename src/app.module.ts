4ecfd5fb3e8e508a76b0eb6cf1a14d4d944072e5
Connie Leung <connie.leung@diginex.com>
2023-11-22 21:24:25 +0800

    refactor: use ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AnimalModule } from './animal/animal.module';
 import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 import { GlobalCacheModule } from './configs/cache.config';
+import redisConfig from './configs/redis.config';
 
 @Module({
-  imports: [GlobalCacheModule, AnimalModule],
+  imports: [
+    GlobalCacheModule,
+    AnimalModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [redisConfig],
+    }),
+  ],
   controllers: [AppController],
-  // providers: [AppService],
 })
 export class AppModule {}
--- src/configs/cache.config.ts
+++ src/configs/cache.config.ts
@@ -1,15 +1,27 @@
 import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigService } from '@nestjs/config';
 import { redisStore } from 'cache-manager-redis-yet';
 import { RedisClientOptions } from 'redis';
-import { env } from './env.config';
+
+interface RedisConfig {
+  host: string;
+  port: number;
+  ttl: number;
+}
 
 // https://github.com/dabroek/node-cache-manager-redis-store/issues/40
-export const GlobalCacheModule = CacheModule.register<RedisClientOptions>({
-  store: redisStore,
-  socket: {
-    host: env.REDIS.HOST,
-    port: env.REDIS.PORT,
+export const GlobalCacheModule = CacheModule.registerAsync<RedisClientOptions>({
+  useFactory: (configService: ConfigService) => {
+    const { host, port, ttl } = configService.get<RedisConfig>('redis');
+    return {
+      store: redisStore,
+      socket: {
+        host,
+        port,
+      },
+      ttl,
+    };
   },
-  ttl: env.REDIS.TTL,
+  inject: [ConfigService],
   isGlobal: true,
 });
